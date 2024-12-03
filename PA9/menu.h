c61910ba4e781be02319fb14f4be939062916e30
ItsJayBlitz <167030039+ItsJayBlitz@users.noreply.github.com>
2024-12-03 14:24:34 -0800

    Create menu.h

---
+++ PA9/menu.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef MENU_H
+#define MENU_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+
+class Menu {
+public:
+    Menu(float width, float height);
+    void draw(sf::RenderWindow& window);
+    int handleMouseInput(const sf::Vector2i& mousePosition, bool clicked);
+
+private:
+    static constexpr int MENU_ITEMS = 3;
+    std::vector<sf::RectangleShape> buttons;
+    std::vector<sf::Text> buttonTexts;
+    sf::Font font;
+    int hoveredIndex;
+};
+
+#endif
