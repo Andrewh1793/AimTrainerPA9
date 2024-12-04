9be7bdf50292dff020136fa236e21b9822139a27
ZacharyMarseglia <146797057+ZacharyMarseglia@users.noreply.github.com>
2024-12-04 14:19:33 -0800

    Update main.cpp

--- PA9/main.cpp
+++ PA9/main.cpp
@@ -1,3 +1,14 @@
+/*******************************************************************************************
+* Programmers: Andrew Hazelbaker, Zachary Marseglia, and Jonathan Jaycob Bautista
+* Class: CptS 122, Fall 2024; Lab Section 3
+* Programming Assignment: PA9
+* Date: November 22, 2024
+* Description: This is an Aim Trainer Game the game features two types of targets standard
+targets which reward basic points and special power-up targets which gives bonus points. 
+The challenge is in the quick spotting and clicking on targets before they disappear while
+striving for the highest possible score. The random spawning system and multiple target 
+sizes ensure a fun and engaging experience for players of all skill levels.
+*******************************************************************************************/
 #include <SFML/Graphics.hpp>
 #include "Target.h"
 #include "PowerUpTarget.h"
