93c1a60681476eeacfeee05d5b2df95521c40416
ZacharyMarseglia <146797057+ZacharyMarseglia@users.noreply.github.com>
2024-12-05 20:52:40 -0800

    Update PowerUpTarget.h

--- PA9/PowerUpTarget.h
+++ PA9/PowerUpTarget.h
@@ -4,14 +4,42 @@
 
 #include "Target.h"
 
-//inherits from Target class
+/*************************************************************
+ * Class: PowerUpTarget                                      *
+ * Description: Represents a specialized target with power-up*
+ * functionality, inheriting from the Target class.          *
+ *************************************************************/
 class PowerUpTarget : public Target {
 public:
-    //constructor
+    /*********************************************************
+     * Function: PowerUpTarget(float x, float y, float r)    *
+     * Description: Constructor that initializes the power-up*
+     * target with position and radius.                      *
+     * Input parameters: x (float), y (float), r (float)     *
+     * Returns: None                                         *
+     * Preconditions: None                                   *
+     * Postconditions: Power-up target is initialized.       *
+     *********************************************************/
     PowerUpTarget(float x, float y, float r);
-    //Function to draw the power up target to the screen
+
+    /*********************************************************
+     * Function: draw(sf::RenderWindow& window)              *
+     * Description: Draws the power-up target on the screen. *
+     * Input parameters: window (sf::RenderWindow&)          *
+     * Returns: None                                         *
+     * Preconditions: Window is initialized.                 *
+     * Postconditions: Power-up target is drawn on the window.*
+     *********************************************************/
     void draw(sf::RenderWindow& window) override;
-    //destructor
+
+    /*********************************************************
+     * Function: ~PowerUpTarget()                            *
+     * Description: Destructor for cleanup of power-up target.*
+     * Input parameters: None                                *
+     * Returns: None                                         *
+     * Preconditions: None                                   *
+     * Postconditions: Object is properly destructed.        *
+     *********************************************************/
     ~PowerUpTarget() override = default;
 };
 
